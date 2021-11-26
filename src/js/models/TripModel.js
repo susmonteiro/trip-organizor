20692fc27d652e592b50caf958f304729434878d
fergascod <fergascod@gmail.com>
2021-11-26 14:03:13 +0100

    Basic Model

--- src/js/models/TripModel.js
+++ src/js/models/TripModel.js
@@ -1,15 +1,46 @@
-class TripModel{
-    constructor(){
-        this.observers - Array
-        this.tripName    
-        this.tripDateBegin
-        this.tripDateEnd
-        this.tripCity
-        this.tripCountry
-        this.tripCoord
-        this.tripFinished   
-        this.attractions - Array
-        this.attrCurrent
+export default class TripModel {
+  constructor(
+    title = null,
+    dateBegin = null,
+    dateEnd = null,
+    coord = [null, null],
+    finished = false,
+    attractions = [],
+    attrCurrent = null
+  ) {
+    this.observers = [];
+    this.setTitle(title);
+    this.setDateBegin(dateBegin);
+    this.setDateEnd(dateEnd);
+    this.setCoord(coord);
+    this.setFinished(finished);
+    this.setAttractions(attractions);
+    this.setAttrCurrent(attrCurrent);
+  }
 
-    }
+  setTitle(title) {
+    this.title = title;
+  }
+  setDateBegin(dateBegin) {
+    this.dateBegin = dateBegin;
+  }
+  setDateEnd(dateEnd) {
+    this.dateEnd = dateEnd;
+  }
+  setCoord(coord) {
+    this.coord = coord;
+  }
+  setFinished(finished) {
+    this.finished = finished;
+  }
+  setAttractions(attractions) {
+    this.attractions = [...attractions];
+  }
+  setAttrCurrent(attrCurrent) {
+    this.attrCurrent = attrCurrent;
+  }
+
+  addAttraction(attraction) {
+    this.attractions = [...this.attractions, attraction];
+  }
 }
