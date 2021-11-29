8b93b647d65bae690657c1f80b25aa7c6f719e0d
fergascod <fergascod@gmail.com>
2021-11-29 11:37:41 +0100

    Set a user with a trip with several attractions

--- src/index.js
+++ src/index.js
@@ -2,6 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 import App from './App';
+import TripModel from './js/models/TripModel.js';
+import AttractionModel from './js/models/AttractionModel.js';
+import UserModel from './js/models/UserModel.js';
+
+import SitesSource from './sitesSource.js';
+
+const trip = new TripModel();
+
+SitesSource.getCoords('Stockholm').then((coords) =>
+  SitesSource.getSites(50, coords.lat, coords.lon).then((sites) =>
+    sites.features.map((site) => {
+      const attr = new AttractionModel({
+        attrID: site.properties.xid,
+        attrName: site.properties.name,
+        attrCoord: site.geometry.coordinates
+      });
+      trip.addAttraction(attr);
+    })
+  )
+);
+
+const user = new UserModel(null, [trip]);
+
+console.log(user);
 
 ReactDOM.render(
   <React.StrictMode>
