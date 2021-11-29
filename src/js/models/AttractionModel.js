6e3ec424b152ef474a178b55fef94ee3d3862da2
fergascod <fergascod@gmail.com>
2021-11-29 11:39:12 +0100

    Prettier doesn't give warnings

--- src/js/models/AttractionModel.js
+++ src/js/models/AttractionModel.js
@@ -1,53 +1,60 @@
-class AttractionModel{
-    constructor(attrID = null, attrName = null, attrCoord = [null, null], attrIsFav = false, attrFinished = false,
-                attrOnMap = false, attrDate = null, attrNotes = null, attrType = null){
-        this.observers = [];        
-        this.setID(attrID);
-        this.setName(attrName);
-        this.setCoord(attrCoord);
-        this.setIsFav(attrIsFav);
-        this.setFinished(attrFinished);
-        this.setOnMap(attrOnMap);
-        this.setDate(attrDate);
-        this.setNotes(attrNotes);
-        this.setType(attrType);
-    }
+export default class AttractionModel {
+  constructor({
+    attrID = null,
+    attrName = null,
+    attrCoord = [null, null],
+    attrIsFav = false,
+    attrFinished = false,
+    attrOnMap = false,
+    attrDate = null,
+    attrNotes = null,
+    attrType = null
+  }) {
+    this.observers = [];
+    this.setID(attrID);
+    this.setName(attrName);
+    this.setCoord(attrCoord);
+    this.setIsFav(attrIsFav);
+    this.setFinished(attrFinished);
+    this.setOnMap(attrOnMap);
+    this.setDate(attrDate);
+    this.setNotes(attrNotes);
+    this.setType(attrType);
+  }
 
-    setID(id){
-        this.attrID = id;
-    }
+  setID(id) {
+    this.attrID = id;
+  }
 
-    setName(attrName){
-        this.attrName = attrName;
-    }
+  setName(attrName) {
+    this.attrName = attrName;
+  }
 
-    setCoord(attrCoord){
-        this.attrCoord = attrCoord;
-    }
+  setCoord(attrCoord) {
+    this.attrCoord = attrCoord;
+  }
 
-    setIsFav(attrIsFav){
-        this.attrIsFav = attrIsFav;
-    }
+  setIsFav(attrIsFav) {
+    this.attrIsFav = attrIsFav;
+  }
 
-    setFinished(attrFinished){
-        this.attrFinished = attrFinished;
-    }
+  setFinished(attrFinished) {
+    this.attrFinished = attrFinished;
+  }
 
-    setOnMap(attrOnMap){
-        this.attrOnMap = attrOnMap;
-    }
+  setOnMap(attrOnMap) {
+    this.attrOnMap = attrOnMap;
+  }
 
-    setDate(attrDate){
-        this.attrDate = attrDate;
-    }
+  setDate(attrDate) {
+    this.attrDate = attrDate;
+  }
 
-    setNotes(attrNotes){
-        this.attrNotes = attrNotes;
-    }
+  setNotes(attrNotes) {
+    this.attrNotes = attrNotes;
+  }
 
-    setType(attrType){
-      this.attrType = attrType;
-    }
-
-
+  setType(attrType) {
+    this.attrType = attrType;
+  }
 }
