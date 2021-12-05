d23ace060acdc70643baa2bb3bd13cef4208d0a1
Paulo Hernández <pauloh@kth.se>
2021-12-05 19:50:49 +0100

    Change of the index.js to previous version

--- src/index.js
+++ src/index.js
@@ -12,30 +12,24 @@ import persistModel from './js/models/FirebaseModel';
 
 import SitesSource from './sitesSource.js';
 
-const trip = new TripModel();
-trip.setTitle("Stockholm fantasy!")
-const user = new UserModel(false, [trip]);
-SitesSource.getCoords('Stockholm', 'SE').then((coords) => {
-  user.setTripCoord([coords.lat, coords.lon]);
-  SitesSource.getSites(50, coords.lat, coords.lon)
+const MyModel = new TripModel();
+
+SitesSource.getCoords('Barcelona', 'ES').then((coords) => {
+  MyModel.setCoord([coords.lat, coords.lon]);
+  SitesSource.getSites(1000, coords.lat, coords.lon)
     .then((sites) => {
       sites.features.map((site) => {
-        console.log(site)
         const attr = new AttractionModel({
           attrID: site.properties.xid,
           attrName: site.properties.name,
           attrCoord: site.geometry.coordinates
         });
-        console.log(attr)
-        user.addAttractionToTrip(attr);
-        console.log(user)
+        MyModel.addAttraction(attr);
       });
-      //MyModel.setTitle("Stockholm")
     })
     .then(() => {
-      console.log(user)
-
-      //persistModel(user);
+      const user = new UserModel(false, []);
+      persistModel(user);
       ReactDOM.render(
         <StyledEngineProvider injectFirst>
           <React.StrictMode>
