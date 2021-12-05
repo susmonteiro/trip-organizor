72ef42901fc4bcc08d79f514604cf7b223d6c4c6
fergascod <fergascod@gmail.com>
2021-12-05 18:55:17 +0100

    Readded routing

--- src/App.js
+++ src/App.js
@@ -42,10 +42,18 @@ function App(props) {
   return (
     <ThemeProvider theme={theme}>
       <Stack spacing={2}>
-        <div className="row">
-          <MainLeftPresenter model={props.model} />
-          <MapPresenter model={props.model} />
-        </div>
+      <TopBarPresenter />
+        <Routes>
+          <Route path="/" element={<HomePresenter />} />
+          <Route path="/trips" element={<TripListPresenter model={props.model} />} />
+          <Route path="/newTrip" element={<AddTripPresenter model={props.model} />} />
+          <Route path="/attractions" element={
+            <div className="row">
+              <MainLeftPresenter model={props.model} />
+              <MapPresenter model={props.model} />
+            </div>} />
+          <Route path="/search" element={<SearchPresenter model={props.model} />} />
+        </Routes>
       </Stack>
     </ThemeProvider>
   );
