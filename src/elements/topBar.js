db7db7460972ac113d1917475888ea3fae9adc30
susanamm <susanamm@kth.se>
2021-12-13 17:49:16 +0100

    Fix back buttons

--- src/elements/topBar.js
+++ src/elements/topBar.js
@@ -7,10 +7,12 @@ export default function TopBar(props) {
   return (
     <Grid container spacing={0} justifyContent="space-between" alignItems="center">
       <Grid item xs={6}>
-        <BackButton href={props.href} onClick={props.onClick}></BackButton>
+        <BackButton href={props.href} onClick={props.onClick}>
+          {props.children}
+        </BackButton>
       </Grid>
       <Grid item xs={6}>
-        <BasicSpeedDial {...props}/>
+        <BasicSpeedDial {...props} />
       </Grid>
     </Grid>
   );
